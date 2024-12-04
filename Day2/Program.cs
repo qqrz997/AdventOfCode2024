8f504b89040585afef1cec26535568cf5d91e5a4
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-12-04 21:26:30 +0000

    Day 2

---
+++ Day2/Program.cs
@@ -0,0 +1,42 @@
+﻿var input = File.ReadAllLines("input.txt").Select(ParseLine).ToArray();
+
+Console.WriteLine(PartOne(input));
+Console.WriteLine(PartTwo(input));
+
+return;
+
+static int PartOne(int[][] lines) => lines
+    .Count(IsSafe);
+
+static int PartTwo(int[][] lines) => lines
+    .Count(line => line.Where((_, i) => IsSafe(RemoveElementAt(line, i))).Any());
+
+static int[] ParseLine(string line) =>
+    line.Split(' ').Select(int.Parse).ToArray();
+
+static int[] RemoveElementAt(int[] array, int i) => 
+    [..array[..i], ..array[(i + 1)..]];
+
+static bool IsSafe(int[] line)
+{
+    if (line[0] == line[1])
+    {
+        return false;
+    }
+    
+    int previous = line[0];
+    bool increasing = line[0] < line[1];
+    
+    foreach (var number in line.Skip(1))
+    {
+        if (Math.Abs(number - previous) is > 3 or < 1
+            || (previous > number && increasing) 
+            || (previous < number && !increasing))
+        {
+            return false;
+        }
+        previous = number;
+    }
+    
+    return true;
+}
